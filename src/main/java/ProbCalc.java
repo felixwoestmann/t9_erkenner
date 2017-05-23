c1a89e4abc82082eb5c97a7924dfd2a4cf22f01a
Felix Wöstmann <f.wosti@gmail.com>
2017-05-23 12:56:29 +0200

    Implemented Prob Of String

--- src/main/java/ProbCalc.java
+++ src/main/java/ProbCalc.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by lostincoding on 23.05.17.
  */
@@ -10,26 +12,43 @@ public class ProbCalc {
 
 
     public double probOfChar(char c) {
-      return  getProbOfCharOnLevel(tree.getRoot(),c);
+        return getProbOfCharOnLevel(tree.getRoot(), c);
     }
 
     public double probOfTwoChars(String chars) {
-        if(chars.length()!=2) {
+        if (chars.length() != 2) {
             return -1;
         }
 
-        char c1=chars.charAt(0);
-        char c2=chars.charAt(1);
+        char c1 = chars.charAt(0);
+        char c2 = chars.charAt(1);
 
-        double c1prob=getProbOfCharOnLevel(tree.getRoot(),c1);
-        CrawlerNode c1node=getChildWithCharAsData(tree.getRoot(),c2);
-        double c2prob=getProbOfCharOnLevel(c1node,c2);
+        double c1prob = getProbOfCharOnLevel(tree.getRoot(), c1);
+        CrawlerNode c1node = getChildWithCharAsData(tree.getRoot(), c2);
+        double c2prob = getProbOfCharOnLevel(c1node, c2);
 
-        return c1prob*c2prob;
+        return c1prob * c2prob;
     }
 
     public double probOfString(String string) {
-        return 0.0;
+        if(string.length()>tree.getChunkSize()) {
+            throw new IllegalArgumentException("Length of String is too big");
+        }
+
+
+        char[] chars = string.toCharArray();
+
+        double returnval = 1;
+
+        CrawlerNode actNode = tree.getRoot();
+        for (char c : chars) {
+            double prob = getProbOfCharOnLevel(actNode, c);
+            actNode = getChildWithCharAsData(actNode, c);
+           returnval*=prob;
+        }
+
+
+        return returnval;
     }
 
     public double probOfCharWithDefinedPrefix(String prefix, char c) {
@@ -37,30 +56,34 @@ public class ProbCalc {
     }
 
 
-private double getProbOfCharOnLevel(CrawlerNode node,char c) {
-   double levelcount=-1;
-    for (CrawlerNode crawlerNode : node.getChildren()) {
-        levelcount += crawlerNode.getData().getCount();
-    }
-    //get appearance of char
-    double appearanceOfChar = -1;
-    for (CrawlerNode crawlerNode : node.getChildren()) {
-        if (crawlerNode.getData().getData() == c) {
-            appearanceOfChar = crawlerNode.getData().getCount();
+    private double getProbOfCharOnLevel(CrawlerNode node, char c) {
+        double levelcount = -1;
+        ArrayList<CrawlerNode> children = node.getChildren();
+        if (children == null) {
+         throw new NullPointerException("node has no childs");
         }
+        for (CrawlerNode crawlerNode : children) {
+            levelcount += crawlerNode.getData().getCount();
+        }
+        //get appearance of char
+        double appearanceOfChar = -1;
+        for (CrawlerNode crawlerNode : children) {
+            if (crawlerNode.getData().getData() == c) {
+                appearanceOfChar = crawlerNode.getData().getCount();
+            }
+        }
+
+        return appearanceOfChar / levelcount;
     }
 
-    return  appearanceOfChar/levelcount;
-}
-
-private CrawlerNode getChildWithCharAsData(CrawlerNode node,char c) {
-    for (CrawlerNode crawlerNode : node.getChildren()) {
-        if(crawlerNode.getData().getData()==c) {
-            return crawlerNode;
+    private CrawlerNode getChildWithCharAsData(CrawlerNode node, char c) {
+        for (CrawlerNode crawlerNode : node.getChildren()) {
+            if (crawlerNode.getData().getData() == c) {
+                return crawlerNode;
+            }
         }
+        return null;
     }
-    return null;
-}
 
 
 }
