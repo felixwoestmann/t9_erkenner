9e49c0354554cbd6bc327f5813fc43370a57ce08
Felix Wöstmann <f.wosti@gmail.com>
2017-06-12 17:25:01 +0200

    Added T9 KEyboard

---
+++ src/main/java/t9/T9Keyboard.java
@@ -0,0 +1,78 @@
+package t9;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lostincoding on 12.06.17.
+ */
+public class T9Keyboard {
+
+    public static ArrayList<String> mapButton(char button) throws IllegalArgumentException {
+
+        ArrayList<String> list = new ArrayList<>();
+        switch (button) {
+            case '1':
+                list.add("1");
+                list.add(".");
+                list.add(",");
+                return list;
+            case '2':
+                list.add("2");
+                list.add("a");
+                list.add("b");
+                list.add("c");
+                return list;
+            case '3':
+                list.add("3");
+                list.add("d");
+                list.add("e");
+                list.add("f");
+                return list;
+            case '4':
+                list.add("4");
+                list.add("g");
+                list.add("h");
+                list.add("i");
+                return list;
+            case '5':
+                list.add("5");
+                list.add("j");
+                list.add("k");
+                list.add("l");
+                return list;
+            case '6':
+                list.add("6");
+                list.add("m");
+                list.add("n");
+                list.add("o");
+                return list;
+            case '7':
+                list.add("7");
+                list.add("p");
+                list.add("q");
+                list.add("r");
+                list.add("s");
+                return list;
+            case '8':
+                list.add("8");
+                list.add("t");
+                list.add("u");
+                list.add("v");
+                return list;
+            case '9':
+                list.add("9");
+                list.add("w");
+                list.add("x");
+                list.add("y");
+                list.add("z");
+                return list;
+            case '0':
+                list.add("0");
+                list.add(" ");
+                return list;
+
+            default:
+                throw new IllegalArgumentException("Zeichen ist nicht bekannt");
+        }
+    }
+}
