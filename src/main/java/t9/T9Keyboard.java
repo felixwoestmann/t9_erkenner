2247689f8b711c17da186c72a89fec1c543df578
Felix Wöstmann <f.wosti@gmail.com>
2017-06-26 14:37:27 +0200

    Removed punctuation from keyboard

--- src/main/java/t9/T9Keyboard.java
+++ src/main/java/t9/T9Keyboard.java
@@ -33,8 +33,8 @@ public class T9Keyboard {
         switch (button) {
             case '1':
                 list.add("1");
-                list.add(".");
-                list.add(",");
+            //    list.add(".");
+              //  list.add(",");
                 return list;
             case '2':
                 list.add("2");
